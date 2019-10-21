6c1b3506c708e754065139867030f74e71fb12c1
Heimer Ahnfelt <thekingofspades@live.se>
2019-10-21 13:54:35 +0200

    Created working ragdoll box with legs, added force to feet and knees

---
+++ Active Ragdoll Project/Assets/MovementController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [Header("Forces")]
+    [SerializeField] private float forwardsForce = 10;
+    [SerializeField] private float upwardForce = 2;
+
+    [Header("Bodyparts")]
+    public Rigidbody[] thighs;
+    public Rigidbody[] knees;
+    public Rigidbody[] shins;
+    public Rigidbody[] feet;
+
+
+    private bool isWalking = false;
+    private float paceTick;
+    private GameObject camera;
+
+
+    private void Start()
+    {
+        camera = Camera.main.gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            FootForward(1);
+
+            Debug.Log("Left Foot forward");
+
+        }
+        if (Input.GetKey("d"))
+        {
+            FootForward(0);
+
+            Debug.Log("Right Foot forward");
+        }
+        //else
+        //{
+        //    isWalking = false;
+        //}
+    }
+
+
+    //private void FixedUpdate()
+    //{
+    //    paceTick = paceTick + Time.fixedDeltaTime;
+    //    if (isWalking)
+    //    {
+    //        if (paceTick > 0.5)
+    //        {
+    //            FootForward(1);
+    //            paceTick = 0f;
+    //        }
+    //    }
+    //}
+
+    private void FootForward(int index)
+    {
+        knees[index].AddForce(camera.transform.forward * forwardsForce + camera.transform.up * upwardForce, ForceMode.Impulse);
+        feet[index].AddForce(camera.transform.forward * forwardsForce, ForceMode.Impulse);
+        Debug.DrawRay(feet[index].transform.position, camera.transform.forward * forwardsForce, Color.red);
+    }
+}
---
+++ Active Ragdoll Project/Assets/Upright.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upright : MonoBehaviour
+{
+    [SerializeField] private bool holdUpright = true;
+    [SerializeField] private float upwardForce = 20;
+    [SerializeField] private float downwardForce = 10; // keep downward force
+    [SerializeField] private float yOffset = 1.5f;
+
+
+    [SerializeField] private Rigidbody rb;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 100; // 
+    }
+
+    private void FixedUpdate()
+    {
+        if (holdUpright)
+        {
+            rb.AddForceAtPosition( Vector3.up * upwardForce,
+                transform.position + transform.TransformPoint(Vector3.up * yOffset),
+                ForceMode.Force); // Upward force and position
+
+            rb.AddForceAtPosition( Vector3.down * downwardForce,
+                transform.position + transform.TransformPoint(Vector3.down * yOffset),
+                ForceMode.Force); // Upward force and position
+
+        }
+    }
+
+}
