b0669f4ce1432b5a8d446ceba99e985847315ace
Heimer Ahnfelt <thekingofspades@live.se>
2019-11-19 12:46:25 +0100

    Tweaked some movement. Added showcase stage, addd moving platform

---
+++ Active Ragdoll Project/Assets/MovingPLatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingPLatform : MonoBehaviour
+{
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 endPos;
+    [SerializeField] private float duration = 5f;
+    private bool isStartPosition = true;
+
+
+    void Start()
+    {
+        startPos = transform.position;
+        MovePlatform();
+    }
+
+    void MovePlatform()
+    {
+        if (isStartPosition)
+        {
+            transform.DOMove(endPos, duration);
+            isStartPosition = false;
+            StartCoroutine("Wait", duration);
+        }
+        else
+        {
+            transform.DOMove(startPos, duration);
+            isStartPosition = true;
+            StartCoroutine("Wait", duration);
+        }
+
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        MovePlatform();
+    }
+
+}
